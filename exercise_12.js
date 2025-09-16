d7bcbc6843c02e8b6e36427c6ca782098a4bc5f7
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-09-16 15:44:18 +0700

    excer 13

--- exercise_12.js
+++ exercise_12.js
@@ -1,9 +1,9 @@
 //Neu chia het cho 3 xuat fizz, cho 5 xuat Buzz va chia het cho 3 lan 5 la FizzBuzz
 function FizzBuzz(number){
-    if(number%3==0 && number%5==0) return 'FizzBuzz';
+    if(number%3===0 && number%5===0) return 'FizzBuzz';
     
-    else if(number%5==0) return 'buzz';
-    else if(number%3==0) return 'fizz';
+    else if(number%5===0) return 'buzz';
+    else if(number%3===0) return 'fizz';
     else if(typeof number !== 'number') return 'NaN';
     else return number;
 }
