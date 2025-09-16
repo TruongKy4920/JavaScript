ae3ad46f74ad9d92edfd80851b424d6337a7ec4f
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-09-16 14:48:58 +0700

    exercise 11

--- for_in_of_loop.js
+++ for_in_of_loop.js
@@ -1,10 +1,20 @@
 let person={
-    name: "ky",
+    name: 23,
     age: 18
 };
-//Với vòng lặp for_of thì cái biến index sẽ đóng vai trò là cái biến giữ giá trị của các key và xuất
-//ra với mỗi lần duyệt 
 
-for(let index of person){
+// for_in có index làm biến duyệt từng key(lấy key làm value) 
+// person[index] -> truy cập vào giá trị của obj bằng key mà index đang giữ
+
+for(let index in person){
+    console.log(index,person[index]);
+}
+let colors=['red','blue'];
+//nếu duyệt mảng xài for_in thì sẽ xuất cái index của phần tử trong mảng
+for(let index in colors){
+    console.log(index);
+}
+//for_of dùng để duyệt mảng và xuất giá trị của từng phần tử
+for(let index of colors){
     console.log(index);
 }
