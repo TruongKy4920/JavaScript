9b445712cfe4e4bc80b99e528fb24f6627eb4fb4
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-09-18 15:11:54 +0700

    obj ex4_5

---
+++ obj_ex4.js
@@ -0,0 +1,11 @@
+const blog_post={
+    title:'a',
+    body:'b',
+    author:'c',
+    views: 10,
+    comments: [{author:'x', body:'y'},
+        {author:'z', body:'q'}] ,
+    isLive: true
+};
+
+console.log(blog_post);
