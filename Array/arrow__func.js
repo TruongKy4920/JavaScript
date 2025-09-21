46767314f03d331b5f4af9d6722891f45e2d458d
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-09-21 14:20:40 +0700

    join_array

--- Array/arrow__func.js
+++ Array/arrow__func.js
@@ -1,5 +1,18 @@
 const courses=[{id: 1, name:'a'},{id: 2, name:'b'}];
-const course=courses.find((course_ele)=>{
+const course=courses.find((course_ele)=>{ //tham số đầu vào của arrow func là từng phần tử của mảng courses
     return course_ele.name==='a';
 });
 console.log(course);
+
+//Cách tạo arrow function
+// B1:bỏ chữ function và chỉ để ngoặc tham số + "=>" 
+// B2:định nghĩa hàm như bth và sử dụng
+function sum(a,b){
+    return a+b;
+}
+(a,b)=>{
+    return a+b;
+} ; //mà làm như này nó sẽ không có chỗ lưu 
+const sum = (a,b)=>{ //nêu sẽ lưu kết quả arrow function trong 1 biến hoặc làm tham số của 1 function khác 
+    return a+b;
+}
