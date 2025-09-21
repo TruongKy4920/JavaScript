848a1387cf562430f99a91062ca3e89ae6372da4
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-09-21 13:32:57 +0700

    arrow & find function

--- Array/find.js
+++ Array/find.js
@@ -1,5 +1,5 @@
 const courses=[{id: 1, name:'a'},{id: 2, name:'b'}];
-const course=courses.find((course_ele)=>{
+const course=courses.find(function(course_ele){ //course_ele tuong chung cho moi phan tu trong array 
     return course_ele.name==='a';
 });
 console.log(course);
