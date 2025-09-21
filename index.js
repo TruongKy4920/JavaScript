ef239977022a578f4400fabc7d09c1295b9dc464
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-09-21 14:41:00 +0700

    every_some

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const nums=[1,3,2,4];
-console.log(nums.sort());
-console.log(nums.reverse());
+const nums=[1,3,-2,4];
+const allPositive= nums.every(num=> {return num>=0;}); 
+console.log(allPositive);
+//method every nay sẽ thực hiện func mình viết cho mọi phần tử
+//Nếu gặp 1 phần tử false với num=>0 -> ngưng duyệt mảng và trả false, nếu đúng hết trả true
+const nums2=[-1,-3,-2,4];
+const atleast1Positive = nums2.some(num=> {return num>=0;});
+console.log(atleast1Positive);
+//giống every về chức năng-> chỉ cần 1 phần tử true với num>=0->ngưng duyệt và trả true.
 
-const nums2= nums.sort((a,b)=>{
-    if(a>b) return 1;
-    if(a<b) return -1;
-    return 0;
-}); //sort tang dan
-const nums3= nums.sort((a,b)=>{
-    if(a>b) return 1;
-    if(a<b) return -1;
-    return 0;
-}); //sort giam dan
-console.log(nums3);
 
-const courses=[{id: 1, name:'a'},{id: 2, name:'b'}];
-const course= courses.sort((a,b)=>{
-    const nameA=a.name.toLowerCase();
-    const nameB=b.name.toLowerCase();
-    if(a.name>b.name) return 1;
-    if(a.name<b.name) return -1;
-    return 0;
-}); //sort tang dan
-console.log(course);
